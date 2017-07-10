de1e62b2d9e399b510ad04fadc71eb8977a9c35d
yatindra <yatindrarao.rao@gmail.com>
2017-07-10 14:04:17 +0530

    Adds add comment feature

--- app.js
+++ app.js
@@ -54,4 +54,16 @@ angular.module('flapperNews', ['ui.router'])
   'posts',
   function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if ($scope.body === '') {
+        return;
+      }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
 }]);
