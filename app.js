e86f736debe1a849fab466d34d38935da90bd59b
yatindra <yatindrarao.rao@gmail.com>
2017-07-10 13:40:06 +0530

    Adds PostCtrl, ui.router and home template

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+    $urlRouterProvider.otherwise('home');
+  }])
 .factory('posts', [function(){
   // service body
   var o = {
@@ -17,14 +34,24 @@ angular.module('flapperNews', [])
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
     }
     $scope.increamentUpvotes = function(post){
-      console.log(post.title);
       post.upvotes += 1;
     }
   }
-]);
+])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+}]);
